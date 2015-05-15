bd05c252a18259f43bb6e48b9e740e8e6268dbd2
Dylan <20769605@student.uwa.edu.au>
2015-05-15 20:16:26 +0800

    modified AES in client package

--- src/ClientServer/AES_Cipher.java
+++ src/ClientServer/AES_Cipher.java
@@ -7,26 +7,34 @@ import javax.crypto.spec.SecretKeySpec;
 
 public class AES_Cipher {
 
-	private String key = "AXQ12745DRT94545"; // 128 bit key
+	private static String key = "AXQ12745DRT94545"; // 128 bit key
 
 	// Create key
-	private Key AESKey = new SecretKeySpec(key.getBytes(), "AES");
+	private static Key AESKey = new SecretKeySpec(key.getBytes(), "AES");
 
-	String encrypt(String text) throws Exception {		// Encryption
-		
+	
+	public static String encrypt(String text) throws Exception {		// Encryption
 		Cipher cipher = Cipher.getInstance("AES");
 		cipher.init(Cipher.ENCRYPT_MODE, AESKey);
 		byte[] encrypted = cipher.doFinal(text.getBytes());
 		return new String(encrypted);
 	}
 
-	String decrypt(String text) throws Exception {		// Decryption
+	public static String decrypt(String text) throws Exception {		// Decryption
 
 		Cipher cipher = Cipher.getInstance("AES");
 		cipher.init(Cipher.DECRYPT_MODE, AESKey);
 		String decrypted = new String(cipher.doFinal(text.getBytes()));
 		return new  String(decrypted);
 	}
+	public static void main(String args[]) throws Exception {
+        String text = "Hello World";
+        System.out.println(text);
+        String encypted = encrypt(text);
+        System.out.println(encypted);
+        String decypted = decrypt(encypted);
+        System.out.println(decypted);
 
-
+        
+	}
 }
